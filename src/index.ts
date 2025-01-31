5a732a6bb3c5017f9b26cd51a5b2a15de5feec28
Utmite <rockimunoz94@gmail.com>
2025-01-31 18:38:04 -0300

    testeo pipeline2

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { Hono } from 'hono'
-
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get('/', (c) => c.text('Hello Cloudflare Workers!'))
+app.get('/api/v1/', (c) => c.json({
+  message: "Hola mundo",
+  date: "2025-01-31",
+}))
 
 export default app
