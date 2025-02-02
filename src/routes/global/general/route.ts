7496283c3ec8adec0318404034dfeb7c447659e1
Utmite <rockimunoz94@gmail.com>
2025-02-02 16:40:58 -0300

    (feat)first test for hono api

--- src/routes/global/general/route.ts
+++ src/routes/global/general/route.ts
@@ -12,21 +12,21 @@ app.get('/career', (c) => {
 app.get('/school', (c) => {
     return c.env.DB.prepare("SELECT DISTINCT school FROM course")
         .all()
-        .then((data) => c.json(data, 200))
+        .then((data) => c.json({ schools: data.results }, 200))
         .catch((error) => c.json({ message: error.message }, 500));
 });
 
 app.get('/category', (c) => {
     return c.env.DB.prepare("SELECT DISTINCT category FROM course")
         .all()
-        .then((data) => c.json(data, 200))
+        .then((data) => c.json({ categorys: data.results }, 200))
         .catch((error) => c.json({ message: error.message }, 500));
 });
 
 app.get('/area', (c) => {
     return c.env.DB.prepare("SELECT DISTINCT area FROM course")
         .all()
-        .then((data) => c.json(data, 200))
+        .then((data) => c.json({ areas: data.results }, 200))
         .catch((error) => c.json({ message: error.message }, 500));
 });
 
---
+++ test/test.general.mjs
@@ -0,0 +1,41 @@
+import { testClient } from 'hono/testing'
+import test from "node:test"
+import assert from 'node:assert'
+
+const TEST_BASE_URL = process.env.TEST_BASE_URL
+
+test("test global general routes", { timeout: 60_000 }, async (t) => {
+    await t.test("schools", async () => {
+        let res = await fetch(`${TEST_BASE_URL}/general/school`)
+        assert.equal(res.status, 200)
+
+        let body = await res.json()
+        assert.ok(body.schools)
+        assert.equal(Array.isArray(body.schools), true)
+        body.schools.forEach(element => {
+            assert.ok(element.school)
+        });
+    })
+    await t.test("category", async () => {
+        let res = await fetch(`${TEST_BASE_URL}/general/category`)
+        assert.equal(res.status, 200)
+
+        let body = await res.json()
+        assert.ok(body.categorys)
+        assert.equal(Array.isArray(body.categorys), true)
+        body.categorys.forEach(element => {
+            assert.ok(element.category !== undefined)
+        });
+    })
+    await t.test("area", async () => {
+        let res = await fetch(`${TEST_BASE_URL}/general/area`)
+        assert.equal(res.status, 200)
+
+        let body = await res.json()
+        assert.ok(body.areas)
+        assert.equal(Array.isArray(body.areas), true)
+        body.areas.forEach(element => {
+            assert.ok(element.area !== undefined)
+        });
+    })
+})
