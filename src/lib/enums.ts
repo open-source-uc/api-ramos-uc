767ac873b5563ec5bbd30c5b7d802f693ddb4e3a
Utmite <rockimunoz94@gmail.com>
2025-02-11 19:47:49 -0300

    temp(auth)

--- src/lib/enums.ts
+++ src/lib/enums.ts
@@ -1,6 +1,6 @@
 export enum PERMISSIONS {
     CREATE_EDIT_OWN_REVIEW = 1,
-    CREATE_USER = 2,
+    AUTH = 2,
     UPDATE_QUOTA = 3,// Para que un bot o un servicio externo agregue cursos, cupos, etc
     ADMIN = 4,
     NULL = -1
